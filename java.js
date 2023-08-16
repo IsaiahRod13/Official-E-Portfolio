e1bbb497fd84fa1b30f355131d82822f07cccb39
Isaiah <isaiahbrodriguez13@gmail.com>
2023-08-16 01:50:36 -0400

    JS 2.0

--- java.js
+++ java.js
@@ -1,1 +1,13 @@
-console.log("hi")
+// template_j8czcu2
+// service_boy0ktj
+//mqApoBt_d546pmUv8
+
+async function contact() {
+  event.preventDefault();
+  await emailjs.sendForm(
+    "service_boy0ktj",
+    "template_j8czcu2",
+    event.target,
+    "mqApoBt_d546pmUv8"
+  )
+}
