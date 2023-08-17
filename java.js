487fcb5e2a7bb4f6398f83dd282e1029bbd2dd31
Isaiah <isaiahbrodriguez13@gmail.com>
2023-08-17 02:13:39 -0400

    JS done 2.47

--- java.js
+++ java.js
@@ -1,6 +1,19 @@
-
 let isModalOpen = false;
-let darkMode = false
+let darkMode = false;
+const scaleFactor = 1 / 20
+
+function moveBackground(event) {
+  const shapes = document.querySelectorAll(".shape");
+  const x = event.clientX * scaleFactor;
+  const y = event.clientY * scaleFactor;
+
+  for (let i = 0; i < shapes.length; ++i) {
+    const isOdd = i % 2 !== 0
+    const boolInt = isOdd ? -1 : 1;
+    shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`;
+  }
+}
+
 function contact(event) {
   event.preventDefault();
   const loading = document.querySelector(".modal__overlay--loading");
@@ -27,18 +40,18 @@ function contact(event) {
 }
 function toggleModal() {
   if (isModalOpen) {
-    isModalOpen = false
+    isModalOpen = false;
     return document.body.classList.remove("modal--open");
   }
-  isModalOpen = true
+  isModalOpen = true;
   document.body.classList += " modal--open";
 }
 
-function toggleDark(){
-  darkMode = !darkMode
-  if(darkMode){
-    document.body.classList += " dark-theme"
-  }else{
-    document.body.classList.remove("dark-theme")
+function toggleDark() {
+  darkMode = !darkMode;
+  if (darkMode) {
+    document.body.classList += " dark-theme";
+  } else {
+    document.body.classList.remove("dark-theme");
   }
 }
